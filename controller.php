18aff6842259702d107fac616b781cef3479833f
hereNT <jeremy.werst@gmail.com>
2013-10-14 18:56:14 -0500

    Now installs a key

--- controller.php
+++ controller.php
@@ -1,11 +1,12 @@
 <?php
+
 defined('C5_EXECUTE') or die("Access Denied.");
 
 class FrontEndPermissionsPackage extends Package {
 
 	protected $pkgHandle = "front_end_permissions";
 	protected $appVersionRequired = "5.6.0";
-	protected $pkgVersion = "0.1";
+	protected $pkgVersion = "0.2";
 
 	public function getPackageName() {
 		return t('Front End Permissions');
@@ -14,18 +15,18 @@ class FrontEndPermissionsPackage extends Package {
 	public function getPackageDescription() {
 		return t('A container for front end permissions');
 	}
-	
+
 	public function install() {
 		$pkg = parent::install();
 
 		$page = SinglePage::add('/dashboard/system/permissions/front_end', $pkg);
 		$page->update(array(
-			'cName' => t('Front End Permissions'),
-			'cDescription' => t('Manage permissions for the front end of the site')
+		    'cName' => t('Front End Permissions'),
+		    'cDescription' => t('Manage permissions for the front end of the site')
 		));
 
 		$pkc = PermissionKeyCategory::add('front_end', $pkg);
-		foreach(array(
+		foreach (array(
 			'group',
 			'user',
 			'group_set',
@@ -35,6 +36,14 @@ class FrontEndPermissionsPackage extends Package {
 			$paet = PermissionAccessEntityType::getByHandle($handle);
 			$pkc->associateAccessEntityType($paet);
 		}
+		PermissionKey::add("front_end", "front_end", t("Front End"), t("Let people do something on the front end"), false, false, $pkg);
+	}
 
+	public function upgrade() {
+		
+	}
+
+	public function on_start() {
+		
 	}
 }
